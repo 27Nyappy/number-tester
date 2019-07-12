b7b227b5b7dd6b1b2e254579499904bf4e9d1c52
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-07-12 16:53:57 -0700

    test passes

--- is-even.js
+++ is-even.js
@@ -2,6 +2,10 @@ function isEven(number) {
     if(number % 2 === 0) {
         return 'Your number is even.';
     }
+    if(number % 2 === 1) {
+        return 'Your number is odd.';
+    }
 }
+
 
 export default isEven;
--- test/tests.js
+++ test/tests.js
@@ -1,10 +1,20 @@
 import isEven from '../is-even.js';
 const test = QUnit.test;
 
-test('labels num as even/odd', function(assert) {
+test('labels num as even', function(assert) {
     const x = 4;
     const y = 2;
     const expected = 'Your number is even.';
+
+    const even = isEven(x, y);
+
+    assert.equal(even, expected);
+})
+
+test('labels num as odd', function(assert) {
+    const x = 5;
+    const y = 2;
+    const expected = 'Your number is odd.';
 
     const even = isEven(x, y);
 
